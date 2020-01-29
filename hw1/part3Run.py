e99e75fcf3758507b914ac10543c333f1dea6341
Alec Xu <Alec_Xu@hotmail.com>
2020-01-29 10:08:39 -0800

    hw finished

--- hw1/part3Run.py
+++ hw1/part3Run.py
@@ -3,20 +3,28 @@ from hw1Functions import *
 print("\nQuestion 1\n")
 # Show the gaussian blurred version of the image with sigma = 6
 sigma = 6
-imPath = '.\\part3images\\0a_cat.bmp'
+imPath = '.\\part3images\\0b_dog.bmp'
 arrBlur = gaussFilterColour(imPath, sigma)
 
 imBlur = Image.fromarray(np.uint8(arrBlur))
-Image.open(imPath).show()
-imBlur.show()
+# Image.open(imPath).show()
+Image.open(imPath).convert('RGB').save('.\\results\\p3q1a.png','PNG')
+# imBlur.show()
+imBlur.convert('RGB').save('.\\results\\p3q1b.png','PNG')
+
 raw_input("press any key to continue...")
 
 print("\nQuestion 2\n")
+
+imPath = '.\\part3images\\0a_cat.bmp'
+arrBlur = gaussFilterColour(imPath, sigma)
+
 # Show the high-pass filtered version of the image, adding 128 for visualization
 arrImage = np.asarray(Image.open(imPath), dtype=np.float32)
 arrHigh =  arrImage - arrBlur + 128
 imHigh = Image.fromarray(np.uint8(arrHigh))
-imHigh.show()
+# imHigh.show()
+imHigh.convert('RGB').save('.\\results\\p3q2.png','PNG')
 raw_input("press any key to continue...")
 
 print("\nQuestion 3\n")
@@ -24,25 +32,34 @@ print("\nQuestion 3\n")
 # Cat dog
 # Show most interesting combinations found for hybrid images
 imHybrid01 = gaussHybridize('.\\part3images\\0a_cat.bmp', '.\\part3images\\0b_dog.bmp' ,3,3)
-Image.fromarray(imHybrid01).show()
+# Image.fromarray(imHybrid01).show()
+Image.fromarray(imHybrid01).convert('RGB').save('.\\results\\p3q3a.png','PNG')
 imHybrid02 = gaussHybridize('.\\part3images\\0a_cat.bmp', '.\\part3images\\0b_dog.bmp' ,5,5)
-Image.fromarray(imHybrid02).show()
+# Image.fromarray(imHybrid02).show()
+Image.fromarray(imHybrid02).convert('RGB').save('.\\results\\p3q3b.png','PNG')
 imHybrid03 = gaussHybridize('.\\part3images\\0a_cat.bmp', '.\\part3images\\0b_dog.bmp' ,7,7)
-Image.fromarray(imHybrid03).show()
+# Image.fromarray(imHybrid03).show()
+Image.fromarray(imHybrid03).convert('RGB').save('.\\results\\p3q3c.png','PNG')
 
 # fish sub
 imHybrid11 = gaussHybridize('.\\part3images\\3b_sub.bmp','.\\part3images\\3a_fish.bmp' ,3,3)
-Image.fromarray(imHybrid11).show()
+# Image.fromarray(imHybrid11).show()
+Image.fromarray(imHybrid11).convert('RGB').save('.\\results\\p3q3d.png','PNG')
 imHybrid12 = gaussHybridize('.\\part3images\\3b_sub.bmp','.\\part3images\\3a_fish.bmp' ,3,7)
-Image.fromarray(imHybrid12).show()
+# Image.fromarray(imHybrid12).show()
+Image.fromarray(imHybrid12).convert('RGB').save('.\\results\\p3q3e.png','PNG')
 imHybrid13 = gaussHybridize('.\\part3images\\3b_sub.bmp','.\\part3images\\3a_fish.bmp' ,5,3)
-Image.fromarray(imHybrid13).show()
+# Image.fromarray(imHybrid13).show()
+Image.fromarray(imHybrid13).convert('RGB').save('.\\results\\p3q3f.png','PNG')
 
 
 # einstein marilyn
 imHybrid21 = gaussHybridize('.\\part3images\\2a_einstein.bmp','.\\part3images\\2b_marilyn.bmp' ,3,3)
-Image.fromarray(imHybrid21).show()
+# Image.fromarray(imHybrid21).show()
+Image.fromarray(imHybrid21).convert('RGB').save('.\\results\\p3q3g.png','PNG')
 imHybrid22 = gaussHybridize('.\\part3images\\2a_einstein.bmp','.\\part3images\\2b_marilyn.bmp' ,5,3)
-Image.fromarray(imHybrid22).show()
+# Image.fromarray(imHybrid22).show()
+Image.fromarray(imHybrid22).convert('RGB').save('.\\results\\p3q3h.png','PNG')
 imHybrid23 = gaussHybridize('.\\part3images\\2a_einstein.bmp','.\\part3images\\2b_marilyn.bmp' ,3,2)
-Image.fromarray(imHybrid23).show()
+# Image.fromarray(imHybrid23).show()
+Image.fromarray(imHybrid23).convert('RGB').save('.\\results\\p3q3i.png','PNG')
--- hw1/part4Run.py
+++ hw1/part4Run.py
@@ -11,47 +11,59 @@ arrGauss = np.asarray(Image.open('.\\part4images\\box_gauss.png'), dtype=np.floa
 
 print("\nSpeck Noise filters\n")
 speckFilteredMedian = cv2.medianBlur(arrSpeck, 5)
-Image.fromarray(speckFilteredMedian).show()
+# Image.fromarray(speckFilteredMedian).show()
+Image.fromarray(speckFilteredMedian).convert('RGB').save('.\\results\\p4q1a.png','PNG')
 
 speckFilteredGauss = cv2.GaussianBlur(arrSpeck, ksize=(5,5), sigmaX=10, sigmaY=10)
-Image.fromarray(speckFilteredGauss).show()
+# Image.fromarray(speckFilteredGauss).show()
+Image.fromarray(speckFilteredGauss).convert('RGB').save('.\\results\\p4q1b.png','PNG')
 
 speckFilteredBi = cv2.bilateralFilter(arrSpeck,15,110,30)
-Image.fromarray(speckFilteredBi).show()
+# Image.fromarray(speckFilteredBi).show()
+Image.fromarray(speckFilteredBi).convert('RGB').save('.\\results\\p4q1c.png','PNG')
     
 print("\nGaussian Noise filters\n")
 GaussFilteredGauss = cv2.GaussianBlur(arrSpeck, ksize=(7,7), sigmaX= 30, sigmaY = 30)
-Image.fromarray(GaussFilteredGauss).show()
+# Image.fromarray(GaussFilteredGauss).show()
+Image.fromarray(GaussFilteredGauss).convert('RGB').save('.\\results\\p4q1d.png','PNG')
 
 GaussFilteredMedian = cv2.medianBlur(arrSpeck, 5)
-Image.fromarray(GaussFilteredMedian).show()
+# Image.fromarray(GaussFilteredMedian).show()
+Image.fromarray(GaussFilteredMedian).convert('RGB').save('.\\results\\p4q1e.png','PNG')
 
 GaussFilteredBi = cv2.bilateralFilter(arrSpeck,20,100,80)
-Image.fromarray(GaussFilteredBi).show()
+# Image.fromarray(GaussFilteredBi).show()
+Image.fromarray(GaussFilteredBi).convert('RGB').save('.\\results\\p4q1f.png','PNG')
 
 raw_input("press any key to continue...")
 
 print("\nQuestion 2\n")
 
 print("\nSpeck Noise filters\n")
-GaussFilteredGauss = cv2.GaussianBlur(arrSpeck, ksize=(7, 7), sigmaX=50)
-Image.fromarray(GaussFilteredGauss).show()
+SpeckFilteredGauss = cv2.GaussianBlur(arrSpeck, ksize=(7, 7), sigmaX=50)
+# Image.fromarray(SpeckFilteredGauss).show()
+Image.fromarray(SpeckFilteredGauss).convert('RGB').save('.\\results\\p4q2a.png','PNG')
 
-GaussFilteredMedian = cv2.medianBlur(arrSpeck,5)
-#Image.fromarray(GaussFilteredMedian).show()
+SpeckFilteredMedian = cv2.medianBlur(arrSpeck,5)
+# Image.fromarray(SpeckFilteredMedian).show()
+Image.fromarray(SpeckFilteredMedian).convert('RGB').save('.\\results\\p4q2b.png','PNG')
 
-GaussFilteredBi = cv2.bilateralFilter(arrSpeck, 7, sigmaColor=150, sigmaSpace=150)
-Image.fromarray(GaussFilteredBi).show()
+SpeckFilteredBi = cv2.bilateralFilter(arrSpeck, 7, sigmaColor=150, sigmaSpace=150)
+# Image.fromarray(SpeckFilteredBi).show()
+Image.fromarray(SpeckFilteredBi).convert('RGB').save('.\\results\\p4q2c.png','PNG')
 
 print("\nGaussian Noise Filters\n")
 GaussFilteredGauss = cv2.GaussianBlur(arrGauss, ksize=(7, 7), sigmaX=50)
-Image.fromarray(GaussFilteredGauss).show()
+# Image.fromarray(GaussFilteredGauss).show()
+Image.fromarray(GaussFilteredGauss).convert('RGB').save('.\\results\\p4q2d.png','PNG')
 
 GaussFilteredMedian = cv2.medianBlur(arrGauss,5)
-#Image.fromarray(GaussFilteredMedian).show()
+# Image.fromarray(GaussFilteredMedian).show()
+Image.fromarray(GaussFilteredMedian).convert('RGB').save('.\\results\\p4q2e.png','PNG')
 
 GaussFilteredBi = cv2.bilateralFilter(arrGauss, 7, sigmaColor=150, sigmaSpace=150)
-Image.fromarray(GaussFilteredBi).show()
+# Image.fromarray(GaussFilteredBi).show()
+Image.fromarray(GaussFilteredBi).convert('RGB').save('.\\results\\p4q2f.png','PNG')
 
 # In both types of noise, the median filter performed the best, although this may be due
 # to the simple geometry. The median filter also introduced jagged edges to the shape.
