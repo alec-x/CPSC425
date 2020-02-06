1ec7433c5b6e5b6709c0558faad05ce8400143d1
Alec Xu <Alec_Xu@hotmail.com>
2020-02-06 15:46:01 -0800

    implemented first  2 questiosn without testing

--- hw2/part1.py
+++ hw2/part1.py
@@ -4,3 +4,30 @@ import math
 from scipy import signal
 import ncc
 
+# Assuming image as PIL Image
+def MakeGaussianPyramid(image, scale, minsize):
+    xLen, yLen = image.size
+    sig = 1.0/(2*scale)
+    gaussianPyramid = []
+    while (min(xLen, yLen >= minsize):
+        image = scipy.ndimage.gaussian_filter(image, sigma=sig)
+        gaussianPyramid.append(image)
+        image = image.resize((int(x*scale),int(y*scale)), Image.BICUBIC)
+        xLen, yLen = image.size
+    return gaussianPyramid
+
+def ShowGaussianPyramid(pyramid):
+    
+    xLenTot = int(float(n)*XLen/(1-1/2^n)) # Using solution to geometric series 
+    yLenTot = int(float(n)*YLen/(1-1/2^n))
+    xOffset = 0
+    yOffset = 0
+    pyrImg = Image.new("L", (xLenTot, yLenTot))
+
+    for im in pyramid:
+        xLen, yLen = im.size
+        xOffset = xOffset + math.ceil(xLen/2)
+        yOffset = yOffset + math.ceil(yLen/2)
+        pyrImg.paste(im,(xOffset, yOffset))
+
+    pyrImg.show()
